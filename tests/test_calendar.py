4ba59ada41078a01f40193283dc883d0bdfc9c76
Daniel <idanielbranham@gmail.com>
2025-09-28 12:22:48 -0400

    unit tests

---
+++ tests/test_calendar.py
@@ -0,0 +1,16 @@
+from datetime import date
+import unittest
+
+import dash
+
+class TestCalendar(unittest.TestCase):
+  def setUp(self):
+    self.app = dash.Dash(__name__)
+    from dashboard.pages import calendar
+
+  def test_first_of_month(self):
+    from dashboard.pages.calendar import first_of_month
+    assert first_of_month(2024, 1) == date(2024, 1, 1)
+
+  def tearDown(self):
+    pass
